1b70f8c735e7d3f9beaa9004f794069f4b698a75
darya <dashka_lala@mail.ru>
2017-07-06 22:13:23 +0300

    Карта

--- web/js/pagination.js
+++ web/js/pagination.js
@@ -1,26 +1,38 @@
 document.addEventListener('DOMContentLoaded', function (e) {
     var chedulePage = document.getElementById("chedulePage");
-    var placePage = document.getElementById("placePage");
+    var placePageMetro = document.getElementById("placePageMetro");
+    var placePageTechnopolis = document.getElementById("placePageTechnopolis");
     var fullChedulePage = document.getElementById("fullChedulePage");
 
     var cheduleLink = document.getElementById("schedule");
-    var placeLink = document.getElementById("place");
+    var placeLink1 = document.getElementById("placeMetro");
+    var placeLink2 = document.getElementById("placeTechnopolis");
     var fullCheduleLink = document.getElementById("fullChedule");
 
     cheduleLink.addEventListener('click', function(e) {
-        placePage.style.display = 'none';
+        placePageMetro.style.display = 'none';
+        placePageTechnopolis.style.display = 'none';
         fullChedulePage.style.display = 'none';
         chedulePage.style.display = 'block';
     });
 
-    placeLink.addEventListener('click', function(e) {
-        placePage.style.display = 'block';
+    placeLink1.addEventListener('click', function(e) {
+        placePageMetro.style.display = 'block';
+        placePageTechnopolis.style.display = 'none';
+        fullChedulePage.style.display = 'none';
+        chedulePage.style.display = 'none';
+    });
+
+    placeLink2.addEventListener('click', function(e) {
+        placePageMetro.style.display = 'none';
+        placePageTechnopolis.style.display = 'block';
         fullChedulePage.style.display = 'none';
         chedulePage.style.display = 'none';
     });
 
     fullCheduleLink.addEventListener('click', function(e) {
-        placePage.style.display = 'none';
+        placePageMetro.style.display = 'none';
+        placePageTechnopolis.style.display = 'none';
         fullChedulePage.style.display = 'block';
         chedulePage.style.display = 'none';
     });
