f3fb0cc0f65261e95a473002595747dcf0754752
darya <dashka_lala@mail.ru>
2017-07-13 03:04:34 +0300

    fullSchedule-version 1.0 + подправила скролл на стартовой странице. А теперь можно и спать пойти

--- src/main/web/js/filling.js
+++ src/main/web/js/filling.js
@@ -8,14 +8,52 @@ document.addEventListener('DOMContentLoaded', function (e) {
 
     var from = json['toOffice'];
     var firstList = document.getElementsByClassName('curTimetable')[0];
+
+    //для полного расписания
+    var fullFromMetroList = document.getElementsByClassName('column')[0];
+    var hourCounter = 7;
+    var row = document.createElement("div");
+    row.className = "row timeLine";
+
     for (var i = 0; i < from.length; i++) {
         var time = from[i]["time"];
         var mask = from[i]["mask"];
+
+        //здесь добавляем элементы на текущее расписание
         var element = document.createElement("li");
 
         element.appendChild(document.createTextNode(time));
         firstList.appendChild(element);
+
+        //а вот тут в полное
+        if (parseInt(time[0]+time[1]) > hourCounter){
+            row = document.createElement("div");
+            row.className = "row timeLine";
+            hourCounter++;
+            var row1 = document.createElement("div");
+            row1.className = "row timeLine";
+            row1.style = "height:4px; width = 100%; background-color: #ccc;padding-right: 20px;";
+            fullFromMetroList.appendChild(row1);
+        }
+        var box = document.createElement("div");
+        box.className = "timeBox";
+        switch (parseInt(mask)){
+            case 31:
+                box.appendChild(document.createTextNode(time));
+                break;
+            case 16:
+                box.appendChild(document.createTextNode(time + "(пт)"));
+                break;
+        }
+
+        row.appendChild(box);
+        fullFromMetroList.appendChild(row);
     }
+
+    var fullFromTechList = document.getElementsByClassName('column')[1];
+    hourCounter = 9;
+    row = document.createElement("div");
+    row.className = "row timeLine";
 
     var to = json["fromOffice"];
     var secondList = document.getElementsByClassName('curTimetable')[1];
@@ -26,5 +64,28 @@ document.addEventListener('DOMContentLoaded', function (e) {
 
         element.appendChild(document.createTextNode(time));
         secondList.appendChild(element);
+
+        if (parseInt(time[0]+time[1]) > hourCounter){
+            row = document.createElement("div");
+            row.className = "row timeLine";
+            hourCounter++;
+            row1 = document.createElement("div");
+            row1.className = "row timeLine";
+            row1.style = "height:4px; width = 100%; background-color: #ccc;padding-right: 20px;";
+            fullFromTechList.appendChild(row1);
+        }
+        box = document.createElement("div");
+        box.className = "timeBox";
+        switch (parseInt(mask)){
+            case 31:
+                box.appendChild(document.createTextNode(time));
+                break;
+            case 16:
+                box.appendChild(document.createTextNode(time + "(пт)"));
+                break;
+        }
+
+        row.appendChild(box);
+        fullFromTechList.appendChild(row);
     }
 });
