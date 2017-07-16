76d158cb5d3c81aee3afacee650e9be8dfbe9171
kerneq <stasik-0@yandex.ru>
2017-07-17 01:27:12 +0300

    add Access-Control-Allow-Origin header to Update Servlet

--- src/main/java/servlets/UpdateServlet.java
+++ src/main/java/servlets/UpdateServlet.java
@@ -21,6 +21,7 @@ public class UpdateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
         sheetsService.updateJson();
         resp.setStatus(HttpServletResponse.SC_OK);
     }
